92c599244122732c7692962ccbbcadf0debccef3
Amber Williams <awilliams1234@live.com>
2020-03-07 19:45:14 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,47 @@
+class TicTacToe 
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+    ]
+    
+  def initialize 
+    @board = [" "," "," "," "," "," "," "," "," "] #Array.new(9, "")
+  end 
+  
+  def display_board 
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end 
+   
+   def input_to_index(str_input)
+     str_input.to_i - 1
+   end 
+   
+   def move(idx, char)
+     @board[idx] = char
+   end 
+   
+   def position_taken?(idx)
+     if @board[idx] == " "
+       return false 
+     else 
+       return true 
+     end 
+     #(@board[idx] == " ") ? false : true 
+   end 
+     
+     def valid_move?(idx)
+       !(self.position_taken?(idx))
+     end 
+       
+end 
+   
